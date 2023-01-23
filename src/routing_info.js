bf982f30ac1377e58232445dc698187557fffd14
Matthew <loganmatthewdev@gmail.com>
2023-01-23 14:48:37 -0800

    added routing info

---
+++ src/routing_info.js
@@ -0,0 +1,12 @@
+exports.create = { 
+  grocery: `http://10.0.0.61:3001/grocery/createNewTask`,
+  todo: `http://10.0.0.61:3001/todo/createNewTask`
+}
+exports.delete = {
+  grocery: `http://10.0.0.61:3001/grocery/delete`,
+  todo: `http://10.0.0.61:3001/todo/delete`
+}
+exports.get = {
+  grocery: `http://10.0.0.61:3001/grocery/fetchTasks`,
+  todo: `http://10.0.0.61:3001/todo/fetchTasks`
+}
