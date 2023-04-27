bed7dd56faba462ebfa60af4992fcecc0c6e4651
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2023-04-26 21:31:31 -0700

    Added ability to make new lists

--- src/components/AppNav/AppNav.js
+++ src/components/AppNav/AppNav.js
@@ -4,28 +4,56 @@ import axios from "axios";
 import vals from '../../routing_info.js'
 
 export const AppNav = () => {
-  const {setTaskType, session} = React.useContext(hookContext);
+  const { setTaskType, session } = React.useContext(hookContext);
   const [lists, setLists] = React.useState([]);
   const handleChange = (evt) => {
     setTaskType(evt.target.value);
   }
-  React.useEffect(()=>{
-    (async() => {
+  const handleNewList = async () => {
+    try {
+      const newList = prompt("New List Name:");
+      if (newList) {
+        const config = {
+          headers: {
+            'Authorization': `Bearer ${session}`
+          }
+        }
+        const response = await axios.patch(`${vals.root}/api/userlists`, { 'newItem': newList }, config);
+        if (response.status === 201) {
+          const temp = [...lists];
+          temp.push(newList);
+          setLists(temp)
+        }
+      }
+    } catch (ex) {
+      console.log(ex);
+    }
+  }
+  React.useEffect(() => {
+    (async () => {
       const config = {
         headers: {
           'Authorization': `Bearer ${session}`
         }
       }
-      const {data} = await axios.get(`${vals.root}/api/userlists`, config);
+      const { data } = await axios.get(`${vals.root}/api/userlists`, config);
       setLists(data);
     })();
-  },[session])
+  }, [session])
   return (
-    <select className={"navSelect"} onChange={handleChange}>
-      <ListOption item={""} key={0} />
-      {lists.map((item,index)=><ListOption item={item} key={index +1} />)}
-    </select>
+    <>
+      <input
+        type={"button"}
+        value={"Create New List"}
+        className={"navBarButton"}
+        onClick={handleNewList}
+      />
+      <select className={"navSelect"} onChange={handleChange}>
+        <ListOption item={""} key={0} />
+        {lists.map((item, index) => <ListOption item={item} key={index + 1} />)}
+      </select>
+    </>
   )
 }
 
-const ListOption = ({item, index}) => <option value={item} className={"navOption"}>{item}</option> 
+const ListOption = ({ item, index }) => <option value={item} className={"navOption"}>{item}</option> 
