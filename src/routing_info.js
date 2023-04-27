ba146cdc5ea4f4e5892a95828774549719824013
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2023-04-26 21:36:46 -0700

    put route back to api

--- src/routing_info.js
+++ src/routing_info.js
@@ -1,2 +1,2 @@
-// exports.root = "https://localnewstv-todo.onrender.com"
-exports.root = "http://10.0.0.60:3006"
+exports.root = "https://localnewstv-todo.onrender.com"
+// exports.root = "http://10.0.0.60:3006"
